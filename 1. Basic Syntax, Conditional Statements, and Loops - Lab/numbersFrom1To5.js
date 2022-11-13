3220d91fa117707e637b14455440b1193564424a
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-13 08:35:36 +0200

    Create numbersFrom1To5.js

---
+++ 1. Basic Syntax, Conditional Statements, and Loops - Lab/numbersFrom1To5.js
@@ -0,0 +1,10 @@
+// Write a function that prints all the numbers from 1 to 5 (inclusive) each on a separate line.
+
+function numbersFrom1To5() {
+
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+
+}
+numbersFrom1To5()
