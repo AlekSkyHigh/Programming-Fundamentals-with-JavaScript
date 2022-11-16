190a7e4c1392a80b746beb0e0ab322c1acf87886
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-16 10:57:42 +0200

    Create reverseString.js

---
+++ 1.2. Basic Syntax, Conditional Statements, and Loops - More Exercises/reverseString.js
@@ -0,0 +1,9 @@
+// Write a program that reverses a string and prints it on the console.
+
+function reverseString(str) {
+
+    console.log(str.split("").reverse().join(""));
+}
+reverseString("hello");
+reverseString("SoftUni");
+reverseString("1234")
