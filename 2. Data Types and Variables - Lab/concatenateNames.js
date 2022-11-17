c72c213d47362b0fa2826b26c69740880d0f0cb8
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-17 10:12:37 +0200

    Create concatenateNames.js

---
+++ 2. Data Types and Variables - Lab/concatenateNames.js
@@ -0,0 +1,10 @@
+// Write a function, which receives two names as string parameters and a delimiter. Print the names joined by the delimiter.
+
+function concatenateNames(firstName, lastName, delimiter) {
+
+    console.log(`${firstName}${delimiter}${lastName}`);
+
+}
+concatenateNames('John', 'Smith', '->')
+concatenateNames('Jan', 'White', '<->')
+concatenateNames('Linda', 'Terry', '=>')
