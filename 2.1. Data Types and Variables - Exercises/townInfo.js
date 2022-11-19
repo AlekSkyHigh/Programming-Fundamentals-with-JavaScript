48c9b477e7a63de6887b53d0a7a272ef4d8196cb
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-19 12:38:07 +0200

    Create townInfo.js

---
+++ 2.1. Data Types and Variables - Exercises/townInfo.js
@@ -0,0 +1,9 @@
+// You will be given 3 parameters. The first parameter will be the name of the town (string), the second – the population (number), and the third the area (number). Print the result in the following format:
+//     "Town {town name} has population of {population} and area {area} square km."
+
+function townInfo(town, population, area) {
+
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+}
+townInfo('Sofia', 1286383, 492)
+townInfo('Plovdiv', 1481353, 512)
