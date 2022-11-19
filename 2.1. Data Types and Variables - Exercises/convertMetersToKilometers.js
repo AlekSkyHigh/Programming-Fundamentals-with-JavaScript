89d19276c6f37014242e73452f3da892f5c90424
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-19 12:38:32 +0200

    Create convertMetersToKilometers.js

---
+++ 2.1. Data Types and Variables - Exercises/convertMetersToKilometers.js
@@ -0,0 +1,8 @@
+// You will be given a number that will be the distance in meters. Write a program that converts meters to kilometers formatted to the second decimal point.
+
+function convertMetersToKilometers (meters) {
+
+    console.log((meters / 1000).toFixed(2));
+}
+convertMetersToKilometers(1852)
+convertMetersToKilometers(798)
