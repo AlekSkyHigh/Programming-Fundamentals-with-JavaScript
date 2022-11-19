b1c5d50cc6c43a058a607e8c4dcd2153ba2961cc
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-19 12:37:34 +0200

    Create charsToString.js

---
+++ 2.1. Data Types and Variables - Exercises/charsToString.js
@@ -0,0 +1,9 @@
+// Write a function, which receives 3 parameters. Each parameter is a single character. Combine all the characters into one string and print it on the console.
+
+function charsToString (first, second, third) {
+
+    console.log(first + second + third);
+}
+charsToString('a', 'b', 'c')
+charsToString('%', '2', 'o')
+charsToString('1', '5', 'p')
