a1e7e80c4f13be3a6060f0e12064fdc7b091ca90
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-20 15:23:14 +0200

    Create reversedChars.js

---
+++ 2.1. Data Types and Variables - Exercises/reversedChars.js
@@ -0,0 +1,9 @@
+// Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between them.
+
+function reversedChars(first, second, third) {
+
+    console.log(`${third} ${second} ${first}`);
+
+}
+reversedChars('A', 'B', 'C')
+reversedChars('1', 'L', '&')
