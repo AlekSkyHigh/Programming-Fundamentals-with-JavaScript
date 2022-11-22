4d79c40eeb09a57aa32b40755f013c88fe2a7bc2
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-22 17:05:05 +0200

    Create binaryToDecimal.js

---
+++ 2.2. Data Types and Variables - More Exercises/binaryToDecimal.js
@@ -0,0 +1,11 @@
+// Write a function that reads an 8-bit binary number and converts it to a decimal.
+// The input comes as one string element, representing a binary number.
+// The output should be printed to the console.
+
+function binaryToDecimal(string) {
+
+    console.log(parseInt(string, 2));
+
+}
+binaryToDecimal("00001001")
+binaryToDecimal("11110000")
