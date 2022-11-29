c13aae4cbfc28e908fae67c60a9ae5baafe73324
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-11-29 19:11:34 +0200

    Create mathPower.js

---
+++ 4. Functions - Lab/mathPower.js
@@ -0,0 +1,9 @@
+// Write a function that calculates and print the value of a number raised to a given power:
+
+function mathPower(number, power) {
+
+    console.log(number ** power);
+
+}
+mathPower(2, 8)
+mathPower(3, 4)
