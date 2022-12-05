66920c11096c3a491678be2e9909be7cab850de6
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-12-05 14:36:40 +0200

    Create firstAndLastKNumbers.js

---
+++ 5. Arrays Advanced - Lab/firstAndLastKNumbers.js
@@ -0,0 +1,15 @@
+// Write a function that prints the first k and the last k elements from an array of numbers.
+// The input comes as an array of number elements. The first element represents the number k, all other elements are from the array that needs to be processed.
+// The output is printed on the console on two lines. On the first line, print the first k elements, separated by space. On the second line, print the last k elements, separated by space.
+
+function firstAndLastKNumbers(arr) {
+
+    let k = arr.shift()
+
+    console.log(arr.slice(0, k).join(" "));
+
+    console.log(arr.slice(arr.length - k).join(" "));
+
+}
+firstAndLastKNumbers([2, 7, 8, 9])
+firstAndLastKNumbers([3,6, 7, 8, 9])
