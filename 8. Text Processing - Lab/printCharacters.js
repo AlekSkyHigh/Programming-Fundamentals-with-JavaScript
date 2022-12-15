086c25861dcabf65a5c5fa316bb9c901c759ba7b
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-12-15 15:47:58 +0200

    Create printCharacters.js

---
+++ 8. Text Processing - Lab/printCharacters.js
@@ -0,0 +1,12 @@
+// Write a function that receives a string and prints all the characters on separate lines.
+
+function printCharacters(string) {
+
+    for (let char of string) {
+        console.log(char);
+    };
+
+}
+printCharacters('AWord')
+
+printCharacters('Sentence')
