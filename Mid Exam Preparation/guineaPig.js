e3a8e39745a123733775e75c2edf01b868cfafc9
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-02 10:39:39 +0200

    Update guineaPig.js

--- Mid Exam Preparation/guineaPig.js
+++ Mid Exam Preparation/guineaPig.js
@@ -1,3 +1,16 @@
+// Merry has a guinea pig named Puppy, that she loves very much. Every month she goes to the nearest pet store and buys him everything he needs – food, hay, and cover.
+// On the first three lines, you will receive the quantity of food, hay, and cover, which Merry buys for a month (30 days). On the fourth line, you will receive the guinea pig's weight.
+// Every day Puppy eats 300 gr of food. Every second day Merry first feeds the pet, then gives it a certain amount of hay equal to 5% of the rest of the food. On every third day, Merry puts Puppy cover with a quantity of 1/3 of its weight.
+// Calculate whether the quantity of food, hay, and cover, will be enough for a month.
+// If Merry runs out of food, hay, or cover, stop the program!
+
+// Output
+// •	If the food, the hay, and the cover are enough, print:
+// o	"Everything is fine! Puppy is happy! Food: {excessFood}, Hay: {excessHay}, Cover: {excessCover}."
+// •	If one of the things is not enough, print:
+// o	"Merry must go to the pet store!"
+// The output values must be formatted to the second decimal place!
+
 function guineaPig(inputArray) {
 
     let food = Number(inputArray[0]);
