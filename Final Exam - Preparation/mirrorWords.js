bd5f2ac8448b6b7ce51616dde8811459a4253671
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-07 08:22:42 +0200

    Update mirrorWords.js

--- Final Exam - Preparation/mirrorWords.js
+++ Final Exam - Preparation/mirrorWords.js
@@ -1,3 +1,17 @@
+// On the first line of the input, you will be given a text string. To win the competition, you have to find all hidden word pairs, read them, and mark the ones that are mirror images of each other.
+// First of all, you have to extract the hidden word pairs. Hidden word pairs are:
+// •	Surrounded by "@" or "#" (only one of the two) in the following pattern #wordOne##wordTwo# or @wordOne@@wordTwo@
+// •	At least 3 characters long each (without the surrounding symbols)
+// •	Made up of letters only
+// If the second word, spelled backward, is the same as the first word and vice versa (casing matters!), they are a match, and you have to store them somewhere. Examples of mirror words: 
+// #Part##traP# @leveL@@Level@ #sAw##wAs#
+// •	If you don`t find any valid pairs, print: "No word pairs found!"
+// •	If you find valid pairs print their count: "{valid pairs count} word pairs found!"
+// •	If there are no mirror words, print: "No mirror words!"
+// •	If there are mirror words print:
+// "The mirror words are:
+// {wordOne} <=> {wordtwo}, {wordOne} <=> {wordtwo}, … {wordOne} <=> {wordtwo}"
+
 function mirrorWords(input) {
 
     let pattern = /([@#])(?<word1>[A-Za-z]{3,})\1{2}(?<word2>[A-Za-z]{3,})\1/gm;
