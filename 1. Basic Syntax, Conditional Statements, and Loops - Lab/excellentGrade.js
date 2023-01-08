6fa6e652036826aca165e462885f700ee373e4a0
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-08 08:51:37 +0200

    Update excellentGrade.js

--- 1. Basic Syntax, Conditional Statements, and Loops - Lab/excellentGrade.js
+++ 1. Basic Syntax, Conditional Statements, and Loops - Lab/excellentGrade.js
@@ -1,3 +1,6 @@
+// Write a function that receives a single number and checks if the grade is excellent or not. 
+// If it is, print "Excellent", otherwise print "Not excellent".
+
 function excellentGrade(grade) {
 
     if (grade >= 5.50) {
