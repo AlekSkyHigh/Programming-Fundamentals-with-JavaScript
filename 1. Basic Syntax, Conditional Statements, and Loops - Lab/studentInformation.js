922040702dacb8784f024b61daa7d96155d05f03
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-09 13:02:45 +0200

    Update studentInformation.js

--- 1. Basic Syntax, Conditional Statements, and Loops - Lab/studentInformation.js
+++ 1. Basic Syntax, Conditional Statements, and Loops - Lab/studentInformation.js
@@ -1,3 +1,7 @@
+// You will be given 3 parameters – student name (string), age (number), and average grade (number). Your task is to print all the info about the student in the following format: 
+// `Name: {student name}, Age: {student age}, Grade: {student grade}`
+// Note: The grade should be formatted to the second decimal point.
+
 function studentInformation(name, age, grade) {
 
     console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
