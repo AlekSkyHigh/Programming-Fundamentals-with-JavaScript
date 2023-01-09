50f4ebdf1ef4f0efc5151af221e12fd43111f747
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-09 13:00:39 +0200

    Update numbersFromNTo1.js

--- 1. Basic Syntax, Conditional Statements, and Loops - Lab/numbersFromNTo1.js
+++ 1. Basic Syntax, Conditional Statements, and Loops - Lab/numbersFromNTo1.js
@@ -1,3 +1,6 @@
+// You are given a number num. Write a PHP script that loops through all of the numbers from num to 1 and prints them. 
+// The input comes as a parameter named num. The parameter num will hold a positive integer.
+
 function numbersFromNTo1(n) {
 
     while (n >= 1) {
